cb19ed691c18cf9be21ea70449382854cc201339
Jack Whisler <whisler1@gmail.com>
2025-01-24 13:04:20 -0600

    reformat folders, custom autoloader

--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
 require '../helpers.php';
-require(basePath('Database.php'));
-require(basePath('Router.php'));
+
+spl_autoload_register(function ($class) {
+    $path = basePath('Framework/' . $class . '.php');
+    if (file_exists($path)) {
+        require $path;
+    }
+});
 
 // Instantiating the router
 $router = new Router();
