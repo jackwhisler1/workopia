40e9152e66654dab5d4d28374e6de58266479b5b
Jack Whisler <whisler1@gmail.com>
2025-02-03 16:26:01 -0600

    implements logout

--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -15,7 +15,7 @@ use Framework\Session;
                     <div class="inline">
                         Welcome, <?= Session::get('user')['name'] ?>
                     </div>
-                    <form>
+                    <form method="POST" action="/auth/logout">
                         <button type="submit" class="text-white inline hover:underline">Logout</button>
                     </form>
                     <a
