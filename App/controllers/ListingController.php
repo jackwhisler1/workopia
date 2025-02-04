3558054327e6ee0aad81ef220b1d9afb5f75a313
Jack Whisler <whisler1@gmail.com>
2025-02-04 14:52:39 -0600

    hides edit and delete if not owner

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -198,6 +198,12 @@ class ListingController
             return;
         }
 
+        // Authorization
+        if (!Authorization::isOwner($listing->user_id)) {
+            Session::setFlashMessage('error_message', 'You are not authorized to edit this listing');
+            return redirect('/listings/' . $listing->id);
+        }
+
         loadView('listings/edit', ['listing' => $listing]);
     }
 
@@ -221,6 +227,12 @@ class ListingController
             ErrorController::notFound('Listing not found');
             return;
         }
+
+        // Authorization
+        if (!Authorization::isOwner($listing->user_id)) {
+            Session::setFlashMessage('error_message', 'You are not authorized to delete this listing');
+            return redirect('/listings/' . $listing->id);
+        } 
 
         $allowedFields = [
             'title',
